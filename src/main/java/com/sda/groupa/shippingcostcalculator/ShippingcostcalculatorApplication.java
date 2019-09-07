30b41418f47cb73f6b53a37675661dcf1d6539b2
nowickiark@outlook.com <amrentos103>
2019-09-07 12:32:39 +0200

    The Base

---
+++ src/main/java/com/sda/groupa/shippingcostcalculator/ShippingcostcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sda.groupa.shippingcostcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingcostcalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingcostcalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/groupa/shippingcostcalculator/ShippingcostcalculatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sda.groupa.shippingcostcalculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShippingcostcalculatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
