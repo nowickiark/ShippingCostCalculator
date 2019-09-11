b377e04e28bb361545190c1c5dd2bc6dff152ba5
KorneliaKa <kornelia.kajda@gmail.com>
2019-09-11 20:29:24 +0200

    Version after Cezar's corrections! Everything works. Ready to merge. Again.

--- src/main/java/com/sda/groupa/shippingcostcalculator/border/exception/BorderCrossNotFoundException.java
+++ src/main/java/com/sda/groupa/shippingcostcalculator/border/exception/BorderCrossNotFoundException.java
@@ -3,6 +3,5 @@ package com.sda.groupa.shippingcostcalculator.border.exception;
 public class BorderCrossNotFoundException extends RuntimeException {
     public BorderCrossNotFoundException(String message) {
         super(message);
-
     }
 }
