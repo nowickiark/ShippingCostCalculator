c2ec6ff2c97efd9f0736ec46fdc952d680aec83b
nowickiark@outlook.com <amrentos103>
2019-09-26 22:48:13 +0200

    Small steps into the view. Added connection to bootstrap tamplates. First steps in Spedyot home View Resolved problem with pring security

--- src/main/java/com/sda/groupa/shippingcostcalculator/login/config/SecurityConfig.java
+++ src/main/java/com/sda/groupa/shippingcostcalculator/login/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
@@ -12,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import javax.persistence.Embeddable;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -23,16 +26,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity security){
+        security.ignoring().antMatchers("/css/**","/images/**","/js/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/admin").hasAuthority("ADMIN")
-                .antMatchers("/super").hasAuthority("SUPER_USER")
                 .antMatchers("/**").authenticated()
                 .and()
-                .formLogin().loginPage("/login")
+                .formLogin().loginPage("/login").permitAll()
                 .usernameParameter("username")
                 .passwordParameter("password")
                 .successHandler(myAuthenticationSuccessHandler())
@@ -49,11 +56,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutSuccessUrl("/login?logout");
+
+        /*          .antMatchers("/resources/**").permitAll().anyRequest().permitAll()*/
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()); }
+
+ /*   @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/static/**").anyRequest();
+    }*/
+
+
 
     @Bean
     public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
